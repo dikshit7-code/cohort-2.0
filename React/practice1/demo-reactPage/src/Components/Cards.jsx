aa6704320eb6ccc42b19c7a4f06afdb65167fda5
Dikshit Barman <dikshitbarman309@gmail.com>
2025-12-25 23:56:07 +0530

    second commit

--- React/practice1/demo-reactPage/src/Components/Cards.jsx
+++ React/practice1/demo-reactPage/src/Components/Cards.jsx
@@ -4,6 +4,17 @@ const Cards = () => {
   return (
     <div className='card-section'>
         <div className='card1'>
+          <i class="ri-home-office-fill"></i>
+          <p>Professional hard courts <span> with tournament-grade lighting & climate control </span> — play in perfect conditions in any season.</p>
+          <button>Game mode</button>
+        </div>
+        <div className='card2'>
+          <button>Private & Group lessons</button>
+        </div>
+        <div className='card1'>
+          <i class="ri-home-office-fill"></i>
+          <p>Professional hard courts <span> with tournament-grade lighting & climate control </span> — play in perfect conditions in any season.</p>
+          <button>Game mode</button>
         </div>
     </div>
   )
